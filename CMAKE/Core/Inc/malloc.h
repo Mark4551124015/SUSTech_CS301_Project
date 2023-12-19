73ca94001f5fa46bfe6040f5c5b5bb683a16b0e6
Mark4551124015 <Mark4551124015@gmail.com>
2023-12-19 22:24:01 +0800

    update comments

--- CMAKE/Core/Inc/malloc.h
+++ CMAKE/Core/Inc/malloc.h
@@ -1,17 +1,16 @@
 #ifndef __MALLOC_H
 #define __MALLOC_H
-#include "stm32f1xx.h"
 #include "main.h"
 //////////////////////////////////////////////////////////////////////////////////	 
-//������ֻ��ѧϰʹ�ã�δ���������ɣ��������������κ���;
-//ALIENTEK MiniSTM32������
-//�ڴ���� ��������	   
-//����ԭ��@ALIENTEK
-//������̳:www.openedv.com
-//�޸�����:2017/6/13
-//�汾��V1.0
-//��Ȩ���У�����ؾ���
-//Copyright(C) �������������ӿƼ����޹�˾ 2009-2019
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//ALIENTEK MiniSTM32开发板
+//内存管理 驱动代码	   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//修改日期:2017/6/13
+//版本：V1.0
+//版权所有，盗版必究。
+//Copyright(C) 广州市星翼电子科技有限公司 2009-2019
 //All rights reserved									  
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -19,34 +18,34 @@
 #define NULL 0
 #endif
 
-//�ڴ�����趨.
-#define MEM_BLOCK_SIZE			32  	  						//�ڴ���СΪ32�ֽ�
-#define MEM_MAX_SIZE			35*1024  						//�������ڴ� 35K
-#define MEM_ALLOC_TABLE_SIZE	MEM_MAX_SIZE/MEM_BLOCK_SIZE 	//�ڴ����С
+//内存参数设定.
+#define MEM_BLOCK_SIZE			32  	  						//内存块大小为32字节
+#define MEM_MAX_SIZE			35*1024  						//最大管理内存 35K
+#define MEM_ALLOC_TABLE_SIZE	MEM_MAX_SIZE/MEM_BLOCK_SIZE 	//内存表大小
  
 		 
-//�ڴ����������
+//内存管理控制器
 struct _m_mallco_dev
 {
-	void (*init)(void);				//��ʼ��
-	u8 (*perused)(void);		  	//�ڴ�ʹ����
-	u8 	*membase;					//�ڴ�� 
-	u16 *memmap; 					//�ڴ����״̬��
-	u8  memrdy; 					//�ڴ�����Ƿ����
+	void (*init)(void);				//初始化
+	u8 (*perused)(void);		  	//内存使用率
+	u8 	*membase;					//内存池 
+	u16 *memmap; 					//内存管理状态表
+	u8  memrdy; 					//内存管理是否就绪
 };
-extern struct _m_mallco_dev mallco_dev;	//��mallco.c���涨��
+extern struct _m_mallco_dev mallco_dev;	//在mallco.c里面定义
 
-void mymemset(void *s,u8 c,u32 count);	//�����ڴ�
-void mymemcpy(void *des,void *src,u32 n);//�����ڴ�     
-void mem_init(void);					 //�ڴ������ʼ������(��/�ڲ�����)
-u32 mem_malloc(u32 size);		 		//�ڴ����(�ڲ�����)
-u8 mem_free(u32 offset);		 		//�ڴ��ͷ�(�ڲ�����)
-u8 mem_perused(void);					//���ڴ�ʹ����(��/�ڲ�����) 
+void mymemset(void *s,u8 c,u32 count);	//设置内存
+void mymemcpy(void *des,void *src,u32 n);//复制内存     
+void mem_init(void);					 //内存管理初始化函数(外/内部调用)
+u32 mem_malloc(u32 size);		 		//内存分配(内部调用)
+u8 mem_free(u32 offset);		 		//内存释放(内部调用)
+u8 mem_perused(void);					//得内存使用率(外/内部调用) 
 ////////////////////////////////////////////////////////////////////////////////
-//�û����ú���
-void myfree(void *ptr);  				//�ڴ��ͷ�(�ⲿ����)
-void *mymalloc(u32 size);				//�ڴ����(�ⲿ����)
-void *myrealloc(void *ptr,u32 size);	//���·����ڴ�(�ⲿ����)
+//用户调用函数
+void myfree(void *ptr);  				//内存释放(外部调用)
+void *mymalloc(u32 size);				//内存分配(外部调用)
+void *myrealloc(void *ptr,u32 size);	//重新分配内存(外部调用)
 #endif
 
 
