f59953f074934f73238a70923eb1b5eede7ca3b6
Mark4551124015 <Mark4551124015@gmail.com>
2023-12-19 20:59:17 +0800

    add floatting support

--- CMAKE/Core/Inc/spi.h
+++ CMAKE/Core/Inc/spi.h
@@ -2,20 +2,8 @@
 #define __SPI_H
 #include "stm32f1xx.h"
 #include "main.h"
-//////////////////////////////////////////////////////////////////////////////////	 
-//������ֻ��ѧϰʹ�ã�δ���������ɣ��������������κ���;
-//ALIENTEK STM32F103������
-//SPI��������	   
-//����ԭ��@ALIENTEK
-//������̳:www.openedv.com
-//��������:2017/5/30
-//�汾��V1.0
-//��Ȩ���У�����ؾ���
-//Copyright(C) �������������ӿƼ����޹�˾ 2014-2024
-//All rights reserved									  
-////////////////////////////////////////////////////////////////////////////////// 	
 
-extern SPI_HandleTypeDef SPI1_Handler;  //SPI���
+extern SPI_HandleTypeDef SPI1_Handler; 
 
 void SPI1_Init(void);
 void SPI1_SetSpeed(u8 SPI_BaudRatePrescaler);
