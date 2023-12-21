215ec1e09e9d10fe9f40d4d7f5fd3df5cba73781
Mark4551124015 <mark4551124015@gmail.com>
2023-12-22 02:09:24 +0800

    Minor fixes

--- CMAKE/Core/Src/frameworks/album_scene.cpp
+++ CMAKE/Core/Src/frameworks/album_scene.cpp
@@ -2,12 +2,56 @@
 
 #include <cstdint>
 #include <cstdio>
+#include <string>
 
 #include "../../FATFS/src/ff.h"
 
-
 album_scene_main::album_scene_main(string name, pii pos, pii shape)
     : dpo(name, pos, shape) {
+    this->photo_init();
+    this->add_son(&title);
+    
+    title.setVisbility(true);
+    this->add_son(&photo_info);
+
+    photo_info.setVisbility(true);
+
+    this->add_son(&photo_name);
+    photo_name.setVisbility(true);
+
+    this->add_son(&photo);
+    photo.setVisbility(true);
+}
+
+album_scene_main::~album_scene_main() { myfree(picfileinfo); }
+
+void album_scene_main::update(display_object *father, pii axis) {
+    if (EVENT[REMOTE_KEY] && photo_cnt != 0) {
+        if (remote_press == "RIGHT") {
+            cur_photo = (cur_photo + 1) % photo_cnt;
+            string temp = "Photo Display [" + to_string(cur_photo) + " / " +
+                          to_string(photo_cnt) + "]";
+            this->photo_info.update_str(temp, 16, BLACK, WHITE);
+            this->photo_name.update_str(photo_index[cur_photo], 16, BLACK,
+                                        WHITE);
+            photo.set_image("0:/PICTURE/" + photo_index[cur_photo]);
+        } else if (remote_press == "LEFT") {
+            cur_photo = (cur_photo - 1 + photo_cnt) % photo_cnt;
+            string temp = "Photo Display [" + to_string(cur_photo) + " / " +
+                          to_string(photo_cnt) + "]";
+            this->photo_info.update_str(temp, 16, BLACK, WHITE);
+            photo.set_image("0:/PICTURE/" + photo_index[cur_photo]);
+            this->photo_name.update_str(photo_index[cur_photo], 16, BLACK,
+                                        WHITE);
+        }
+    }
+    if (EVENT[REMOTE_KEY] && photo_cnt == 0) {
+        this->photo_init();
+    }
+    dpo::update(father, axis);
+}
+
+void album_scene_main::photo_init() {
     f_mount(fs[0], "0:", 1);  // 挂载SD卡
     f_mount(fs[1], "1:", 1);  // 挂载FLASH.
     printf("mounted partition\n");
@@ -16,29 +60,26 @@ album_scene_main::album_scene_main(string name, pii pos, pii shape)
     cur_photo = 0;
 
     picfileinfo = (FILINFO *)mymalloc(sizeof(FILINFO));
-    printf("New FILINFO\n");
     picdir = DIR();
-    printf("New Dir\n");
 
     if (picfileinfo == NULL) {
         printf("malloc error\n");
+        return;
     }
     mymemset(picfileinfo, 0, sizeof(FILINFO));
     u8 res;
-    while ((res = f_opendir(&picdir, "0:/PICTURE")))  // 打开图片文件夹
-    {
+    if ((res = f_opendir(&picdir, "0:/PICTURE"))) {
         printf("Wrong Dir 0:/PICTURE! %d\n", (int)res);
-        delay_ms(200);
-    }
+        return;
+    }  // 打开图片文件夹
+
     photo_cnt = pic_get_tnum((uint8_t *)"0:/PICTURE");
-    while (photo_cnt == NULL) {
-        printf("No Picture in 0:/PICTURE !");
-        delay_ms(200);
-    }
-    while (!picfileinfo)  // 内存分配出错
-    {
+
+    printf("No Picture in 0:/PICTURE !");
+
+    if (!picfileinfo) {  // 内存分配出错
         printf("Memory Alloc Failed!");
-        delay_ms(200);
+        return;
     }
     res = f_opendir(&picdir, "0:/PICTURE");  // 打开目录
 
@@ -53,9 +94,9 @@ album_scene_main::album_scene_main(string name, pii pos, pii shape)
                 break;  // 错误了/到末尾了,退出
             res = f_typetell((u8 *)picfileinfo->fname);
             if ((res & 0XF0) == 0X50)  // 取高四位,看看是不是图片文件
-            {   
+            {
                 string img_name = picfileinfo->fname;
-                photo_index[curindex] = "0:/PICTURE/" + img_name;  // 记录索引
+                photo_index[curindex] = img_name;  // 记录索引
                 printf("Adding %s\n", photo_index[curindex].c_str());
                 curindex++;
             }
@@ -65,22 +106,15 @@ album_scene_main::album_scene_main(string name, pii pos, pii shape)
         printf("Open Dir Failed!\n");
     }
     printf("Done Photo Finding %d\n", photo_cnt);
-    photo.set_image(photo_index[0]);
-    this->add_son(&photo);
-    photo.setVisbility(true);
-}
 
-album_scene_main::~album_scene_main() { myfree(picfileinfo); }
-
-void album_scene_main::update(display_object *father, pii axis) {
-    if (EVENT[REMOTE_KEY]) {
-        if (remote_press=="RIGHT") {
-            cur_photo = (cur_photo + 1) % photo_cnt;
-            photo.set_image(photo_index[cur_photo]);
-        } else if (remote_press=="LEFT") {
-            cur_photo = (cur_photo - 1 + photo_cnt) % photo_cnt;
-            photo.set_image(photo_index[cur_photo]);
-        }
+    if (photo_cnt == 0) {
+        photo_name.update_str("No Photo Found!", 16, BLACK, WHITE);
+        photo_info.update_str("Nothing in your PICTURE/", 16, BLACK, WHITE);
+    } else {
+        photo_name.update_str(photo_index[0], 16, BLACK, WHITE);
+        string temp = "Photo Display [" + to_string(cur_photo) + " / " +
+                      to_string(photo_cnt) + "]";
+        photo_info.update_str(temp, 16, BLACK, WHITE);
+        photo.set_image("0:/PICTURE/" + photo_index[0]);
     }
-    dpo::update(father, axis);
 }
--- CMAKE/Core/Src/stm32f1xx_it.cpp
+++ CMAKE/Core/Src/stm32f1xx_it.cpp
@@ -18,8 +18,12 @@
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
-#include "main.h"
 #include "stm32f1xx_it.h"
+
+#include <cstdio>
+
+#include "main.h"
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "led.h"
@@ -50,7 +54,7 @@ unsigned char rxBuffer[20];
 // extern LED leddev;
 bool rx_flag = false;
 extern u8 RmtSta;
-extern u16 Dval;        // 下降沿时计数器的�?
+extern u16 Dval;    // 下降沿时计数器的�?
 extern u32 RmtRec;  // 红外接收到的数据
 extern u8 RmtCnt;   // 按键按下的次�?
 
@@ -83,130 +87,117 @@ extern chat_scene_main chat_sc;
 /*           Cortex-M3 Processor Interruption and Exception Handlers          */
 /******************************************************************************/
 /**
-  * @brief This function handles Non maskable interrupt.
-  */
-void NMI_Handler(void)
-{
-  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
+ * @brief This function handles Non maskable interrupt.
+ */
+void NMI_Handler(void) {
+    /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
 
-  /* USER CODE END NonMaskableInt_IRQn 0 */
-  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
+    /* USER CODE END NonMaskableInt_IRQn 0 */
+    /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
     while (1) {
     }
-  /* USER CODE END NonMaskableInt_IRQn 1 */
+    /* USER CODE END NonMaskableInt_IRQn 1 */
 }
 
 /**
-  * @brief This function handles Hard fault interrupt.
-  */
-void HardFault_Handler(void)
-{
-  /* USER CODE BEGIN HardFault_IRQn 0 */
+ * @brief This function handles Hard fault interrupt.
+ */
+void HardFault_Handler(void) {
+    /* USER CODE BEGIN HardFault_IRQn 0 */
 
-  /* USER CODE END HardFault_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
-    /* USER CODE END W1_HardFault_IRQn 0 */
-  }
+    /* USER CODE END HardFault_IRQn 0 */
+    while (1) {
+        /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+        /* USER CODE END W1_HardFault_IRQn 0 */
+    }
 }
 
 /**
-  * @brief This function handles Memory management fault.
-  */
-void MemManage_Handler(void)
-{
-  /* USER CODE BEGIN MemoryManagement_IRQn 0 */
+ * @brief This function handles Memory management fault.
+ */
+void MemManage_Handler(void) {
+    /* USER CODE BEGIN MemoryManagement_IRQn 0 */
 
-  /* USER CODE END MemoryManagement_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
-    /* USER CODE END W1_MemoryManagement_IRQn 0 */
-  }
+    /* USER CODE END MemoryManagement_IRQn 0 */
+    while (1) {
+        /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
+        /* USER CODE END W1_MemoryManagement_IRQn 0 */
+    }
 }
 
 /**
-  * @brief This function handles Prefetch fault, memory access fault.
-  */
-void BusFault_Handler(void)
-{
-  /* USER CODE BEGIN BusFault_IRQn 0 */
+ * @brief This function handles Prefetch fault, memory access fault.
+ */
+void BusFault_Handler(void) {
+    /* USER CODE BEGIN BusFault_IRQn 0 */
 
-  /* USER CODE END BusFault_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_BusFault_IRQn 0 */
-    /* USER CODE END W1_BusFault_IRQn 0 */
-  }
+    /* USER CODE END BusFault_IRQn 0 */
+    while (1) {
+        /* USER CODE BEGIN W1_BusFault_IRQn 0 */
+        /* USER CODE END W1_BusFault_IRQn 0 */
+    }
 }
 
 /**
-  * @brief This function handles Undefined instruction or illegal state.
-  */
-void UsageFault_Handler(void)
-{
-  /* USER CODE BEGIN UsageFault_IRQn 0 */
+ * @brief This function handles Undefined instruction or illegal state.
+ */
+void UsageFault_Handler(void) {
+    /* USER CODE BEGIN UsageFault_IRQn 0 */
 
-  /* USER CODE END UsageFault_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_UsageFault_IRQn 0 */
-    /* USER CODE END W1_UsageFault_IRQn 0 */
-  }
+    /* USER CODE END UsageFault_IRQn 0 */
+    while (1) {
+        /* USER CODE BEGIN W1_UsageFault_IRQn 0 */
+        /* USER CODE END W1_UsageFault_IRQn 0 */
+    }
 }
 
 /**
-  * @brief This function handles System service call via SWI instruction.
-  */
-void SVC_Handler(void)
-{
-  /* USER CODE BEGIN SVCall_IRQn 0 */
+ * @brief This function handles System service call via SWI instruction.
+ */
+void SVC_Handler(void) {
+    /* USER CODE BEGIN SVCall_IRQn 0 */
 
-  /* USER CODE END SVCall_IRQn 0 */
-  /* USER CODE BEGIN SVCall_IRQn 1 */
+    /* USER CODE END SVCall_IRQn 0 */
+    /* USER CODE BEGIN SVCall_IRQn 1 */
 
-  /* USER CODE END SVCall_IRQn 1 */
+    /* USER CODE END SVCall_IRQn 1 */
 }
 
 /**
-  * @brief This function handles Debug monitor.
-  */
-void DebugMon_Handler(void)
-{
-  /* USER CODE BEGIN DebugMonitor_IRQn 0 */
+ * @brief This function handles Debug monitor.
+ */
+void DebugMon_Handler(void) {
+    /* USER CODE BEGIN DebugMonitor_IRQn 0 */
 
-  /* USER CODE END DebugMonitor_IRQn 0 */
-  /* USER CODE BEGIN DebugMonitor_IRQn 1 */
+    /* USER CODE END DebugMonitor_IRQn 0 */
+    /* USER CODE BEGIN DebugMonitor_IRQn 1 */
 
-  /* USER CODE END DebugMonitor_IRQn 1 */
+    /* USER CODE END DebugMonitor_IRQn 1 */
 }
 
 /**
-  * @brief This function handles Pendable request for system service.
-  */
-void PendSV_Handler(void)
-{
-  /* USER CODE BEGIN PendSV_IRQn 0 */
+ * @brief This function handles Pendable request for system service.
+ */
+void PendSV_Handler(void) {
+    /* USER CODE BEGIN PendSV_IRQn 0 */
 
-  /* USER CODE END PendSV_IRQn 0 */
-  /* USER CODE BEGIN PendSV_IRQn 1 */
+    /* USER CODE END PendSV_IRQn 0 */
+    /* USER CODE BEGIN PendSV_IRQn 1 */
 
-  /* USER CODE END PendSV_IRQn 1 */
+    /* USER CODE END PendSV_IRQn 1 */
 }
 
 /**
-  * @brief This function handles System tick timer.
-  */
-void SysTick_Handler(void)
-{
-  /* USER CODE BEGIN SysTick_IRQn 0 */
+ * @brief This function handles System tick timer.
+ */
+void SysTick_Handler(void) {
+    /* USER CODE BEGIN SysTick_IRQn 0 */
 
-  /* USER CODE END SysTick_IRQn 0 */
-  HAL_IncTick();
-  /* USER CODE BEGIN SysTick_IRQn 1 */
+    /* USER CODE END SysTick_IRQn 0 */
+    HAL_IncTick();
+    /* USER CODE BEGIN SysTick_IRQn 1 */
 
-  /* USER CODE END SysTick_IRQn 1 */
+    /* USER CODE END SysTick_IRQn 1 */
 }
 
 /******************************************************************************/
@@ -217,172 +208,160 @@ void SysTick_Handler(void)
 /******************************************************************************/
 
 /**
-  * @brief This function handles RTC global interrupt.
-  */
-void RTC_IRQHandler(void)
-{
-  /* USER CODE BEGIN RTC_IRQn 0 */
+ * @brief This function handles RTC global interrupt.
+ */
+void RTC_IRQHandler(void) {
+    /* USER CODE BEGIN RTC_IRQn 0 */
 
-  /* USER CODE END RTC_IRQn 0 */
-  HAL_RTCEx_RTCIRQHandler(&hrtc);
-  /* USER CODE BEGIN RTC_IRQn 1 */
+    /* USER CODE END RTC_IRQn 0 */
+    HAL_RTCEx_RTCIRQHandler(&hrtc);
+    /* USER CODE BEGIN RTC_IRQn 1 */
 
-  /* USER CODE END RTC_IRQn 1 */
+    /* USER CODE END RTC_IRQn 1 */
 }
 
 /**
-  * @brief This function handles EXTI line0 interrupt.
-  */
-void EXTI0_IRQHandler(void)
-{
-  /* USER CODE BEGIN EXTI0_IRQn 0 */
+ * @brief This function handles EXTI line0 interrupt.
+ */
+void EXTI0_IRQHandler(void) {
+    /* USER CODE BEGIN EXTI0_IRQn 0 */
 
-  /* USER CODE END EXTI0_IRQn 0 */
-  HAL_GPIO_EXTI_IRQHandler(KEY_WK_Pin);
-  /* USER CODE BEGIN EXTI0_IRQn 1 */
+    /* USER CODE END EXTI0_IRQn 0 */
+    HAL_GPIO_EXTI_IRQHandler(KEY_WK_Pin);
+    /* USER CODE BEGIN EXTI0_IRQn 1 */
 
-  /* USER CODE END EXTI0_IRQn 1 */
+    /* USER CODE END EXTI0_IRQn 1 */
 }
 
 /**
-  * @brief This function handles DMA1 channel4 global interrupt.
-  */
-void DMA1_Channel4_IRQHandler(void)
-{
-  /* USER CODE BEGIN DMA1_Channel4_IRQn 0 */
+ * @brief This function handles DMA1 channel4 global interrupt.
+ */
+void DMA1_Channel4_IRQHandler(void) {
+    /* USER CODE BEGIN DMA1_Channel4_IRQn 0 */
 
-  /* USER CODE END DMA1_Channel4_IRQn 0 */
-  HAL_DMA_IRQHandler(&hdma_usart1_tx);
-  /* USER CODE BEGIN DMA1_Channel4_IRQn 1 */
+    /* USER CODE END DMA1_Channel4_IRQn 0 */
+    HAL_DMA_IRQHandler(&hdma_usart1_tx);
+    /* USER CODE BEGIN DMA1_Channel4_IRQn 1 */
 
-  /* USER CODE END DMA1_Channel4_IRQn 1 */
+    /* USER CODE END DMA1_Channel4_IRQn 1 */
 }
 
 /**
-  * @brief This function handles DMA1 channel5 global interrupt.
-  */
-void DMA1_Channel5_IRQHandler(void)
-{
-  /* USER CODE BEGIN DMA1_Channel5_IRQn 0 */
+ * @brief This function handles DMA1 channel5 global interrupt.
+ */
+void DMA1_Channel5_IRQHandler(void) {
+    /* USER CODE BEGIN DMA1_Channel5_IRQn 0 */
 
-  /* USER CODE END DMA1_Channel5_IRQn 0 */
-  HAL_DMA_IRQHandler(&hdma_usart1_rx);
-  /* USER CODE BEGIN DMA1_Channel5_IRQn 1 */
+    /* USER CODE END DMA1_Channel5_IRQn 0 */
+    HAL_DMA_IRQHandler(&hdma_usart1_rx);
+    /* USER CODE BEGIN DMA1_Channel5_IRQn 1 */
 
-  /* USER CODE END DMA1_Channel5_IRQn 1 */
+    /* USER CODE END DMA1_Channel5_IRQn 1 */
 }
 
 /**
-  * @brief This function handles EXTI line[9:5] interrupts.
-  */
-void EXTI9_5_IRQHandler(void)
-{
-  /* USER CODE BEGIN EXTI9_5_IRQn 0 */
+ * @brief This function handles EXTI line[9:5] interrupts.
+ */
+void EXTI9_5_IRQHandler(void) {
+    /* USER CODE BEGIN EXTI9_5_IRQn 0 */
 
-  /* USER CODE END EXTI9_5_IRQn 0 */
-  HAL_GPIO_EXTI_IRQHandler(KEY0_Pin);
-  /* USER CODE BEGIN EXTI9_5_IRQn 1 */
+    /* USER CODE END EXTI9_5_IRQn 0 */
+    HAL_GPIO_EXTI_IRQHandler(KEY0_Pin);
+    /* USER CODE BEGIN EXTI9_5_IRQn 1 */
 
-  /* USER CODE END EXTI9_5_IRQn 1 */
+    /* USER CODE END EXTI9_5_IRQn 1 */
 }
 
 /**
-  * @brief This function handles TIM3 global interrupt.
-  */
-void TIM3_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM3_IRQn 0 */
+ * @brief This function handles TIM3 global interrupt.
+ */
+void TIM3_IRQHandler(void) {
+    /* USER CODE BEGIN TIM3_IRQn 0 */
 
-  /* USER CODE END TIM3_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim3);
-  /* USER CODE BEGIN TIM3_IRQn 1 */
+    /* USER CODE END TIM3_IRQn 0 */
+    HAL_TIM_IRQHandler(&htim3);
+    /* USER CODE BEGIN TIM3_IRQn 1 */
 
-  /* USER CODE END TIM3_IRQn 1 */
+    /* USER CODE END TIM3_IRQn 1 */
 }
 
 /**
-  * @brief This function handles I2C1 event interrupt.
-  */
-void I2C1_EV_IRQHandler(void)
-{
-  /* USER CODE BEGIN I2C1_EV_IRQn 0 */
+ * @brief This function handles I2C1 event interrupt.
+ */
+void I2C1_EV_IRQHandler(void) {
+    /* USER CODE BEGIN I2C1_EV_IRQn 0 */
 
-  /* USER CODE END I2C1_EV_IRQn 0 */
-  HAL_I2C_EV_IRQHandler(&hi2c1);
-  /* USER CODE BEGIN I2C1_EV_IRQn 1 */
+    /* USER CODE END I2C1_EV_IRQn 0 */
+    HAL_I2C_EV_IRQHandler(&hi2c1);
+    /* USER CODE BEGIN I2C1_EV_IRQn 1 */
 
-  /* USER CODE END I2C1_EV_IRQn 1 */
+    /* USER CODE END I2C1_EV_IRQn 1 */
 }
 
 /**
-  * @brief This function handles I2C1 error interrupt.
-  */
-void I2C1_ER_IRQHandler(void)
-{
-  /* USER CODE BEGIN I2C1_ER_IRQn 0 */
+ * @brief This function handles I2C1 error interrupt.
+ */
+void I2C1_ER_IRQHandler(void) {
+    /* USER CODE BEGIN I2C1_ER_IRQn 0 */
 
-  /* USER CODE END I2C1_ER_IRQn 0 */
-  HAL_I2C_ER_IRQHandler(&hi2c1);
-  /* USER CODE BEGIN I2C1_ER_IRQn 1 */
+    /* USER CODE END I2C1_ER_IRQn 0 */
+    HAL_I2C_ER_IRQHandler(&hi2c1);
+    /* USER CODE BEGIN I2C1_ER_IRQn 1 */
 
-  /* USER CODE END I2C1_ER_IRQn 1 */
+    /* USER CODE END I2C1_ER_IRQn 1 */
 }
 
 /**
-  * @brief This function handles USART1 global interrupt.
-  */
-void USART1_IRQHandler(void)
-{
-  /* USER CODE BEGIN USART1_IRQn 0 */
+ * @brief This function handles USART1 global interrupt.
+ */
+void USART1_IRQHandler(void) {
+    /* USER CODE BEGIN USART1_IRQn 0 */
 
-  /* USER CODE END USART1_IRQn 0 */
-  HAL_UART_IRQHandler(&huart1);
-  /* USER CODE BEGIN USART1_IRQn 1 */
+    /* USER CODE END USART1_IRQn 0 */
+    HAL_UART_IRQHandler(&huart1);
+    /* USER CODE BEGIN USART1_IRQn 1 */
     HAL_UART_Receive_IT(&huart1, (uint8_t *)rxBuffer, 1);
-  /* USER CODE END USART1_IRQn 1 */
+    /* USER CODE END USART1_IRQn 1 */
 }
 
 /**
-  * @brief This function handles SDIO global interrupt.
-  */
-void SDIO_IRQHandler(void)
-{
-  /* USER CODE BEGIN SDIO_IRQn 0 */
+ * @brief This function handles SDIO global interrupt.
+ */
+void SDIO_IRQHandler(void) {
+    /* USER CODE BEGIN SDIO_IRQn 0 */
+    /* USER CODE END SDIO_IRQn 0 */
+    HAL_SD_IRQHandler(&hsd);
+    /* USER CODE BEGIN SDIO_IRQn 1 */
 
-  /* USER CODE END SDIO_IRQn 0 */
-  HAL_SD_IRQHandler(&hsd);
-  /* USER CODE BEGIN SDIO_IRQn 1 */
-
-  /* USER CODE END SDIO_IRQn 1 */
+    /* USER CODE END SDIO_IRQn 1 */
 }
 
 /**
-  * @brief This function handles DMA2 channel4 and channel5 global interrupts.
-  */
-void DMA2_Channel4_5_IRQHandler(void)
-{
-  /* USER CODE BEGIN DMA2_Channel4_5_IRQn 0 */
+ * @brief This function handles DMA2 channel4 and channel5 global interrupts.
+ */
+void DMA2_Channel4_5_IRQHandler(void) {
+    /* USER CODE BEGIN DMA2_Channel4_5_IRQn 0 */
 
-  /* USER CODE END DMA2_Channel4_5_IRQn 0 */
-  HAL_DMA_IRQHandler(&hdma_sdio);
-  /* USER CODE BEGIN DMA2_Channel4_5_IRQn 1 */
+    /* USER CODE END DMA2_Channel4_5_IRQn 0 */
+    HAL_DMA_IRQHandler(&hdma_sdio);
+    /* USER CODE BEGIN DMA2_Channel4_5_IRQn 1 */
 
-  /* USER CODE END DMA2_Channel4_5_IRQn 1 */
+    /* USER CODE END DMA2_Channel4_5_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
     if (huart->Instance == USART1) {
-        //static char uLength = 0;
+        // static char uLength = 0;
         if (rxBuffer[0] == '\n') {
-            //uLength = 0;
+            // uLength = 0;
             int str_len = RX_DATA.length();
             RX_DATA.erase(str_len - 1);
             rx_flag = 1;
             printf("%s", RX_DATA.c_str());
         } else {
             RX_DATA += rxBuffer[0];
-            //uLength++;
+            // uLength++;
         }
     }
 }
@@ -420,9 +399,10 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 // 0xffff); 		break; 	case KEY1_Pin:
 //             sprintf(output, "KEY1 Pressed");
 // 			HAL_UART_Transmit(&huart1, output, strlen(output),
-// 0xffff); 		break; 	case KEY_WK_Pin: 			HAL_GPIO_TogglePin(LED0_GPIO_Port,
-// LED0_Pin); 			HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin); 			HAL_Delay(100);
-// 			HAL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin);
+// 0xffff); 		break; 	case KEY_WK_Pin:
+// HAL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin);
+// HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
+// HAL_Delay(100); 			HAL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin);
 // 			HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
 // 			HAL_Delay(100);
 // 			HAL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin);
